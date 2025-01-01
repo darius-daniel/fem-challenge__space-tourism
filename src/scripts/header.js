7108d71c946fb9c5600542ed58998f79e5b6fe46
Darius Daniel <dariusdaniel771@email.com>
2025-01-01 13:14:14 +0100

    Fixed issue with multi-page builds

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,62 +1,65 @@
-const hamburgerIcon = document.getElementsByClassName('hamburger-icon')[0];
-const body = document.getElementsByTagName('body')[0];
+const hamburgerIcon = document.getElementsByClassName("hamburger-icon")[0];
+const body = document.getElementsByTagName("body")[0];
 let mobileMenuState = false;
 
 hamburgerIcon.onclick = function () {
-  mobileMenuState = !mobileMenuState;
-  if (mobileMenuState) {
-    const mobileMenu = document.createElement('div');
-    mobileMenu.classList.add('mobile-menu');
+    mobileMenuState = !mobileMenuState;
+    if (mobileMenuState) {
+        const mobileMenu = document.createElement("div");
+        mobileMenu.classList.add("mobile-menu");
 
-    const menuClose = document.createElement('img');
-    menuClose.src = '/shared/icon-close.svg';
-    menuClose.classList.add('mobile-menu__close');
-    menuClose.onclick = function () {
-      mobileMenuState = false;
-      mobileMenu.classList.add('hidden');
-    };
-    mobileMenu.append(menuClose);
+        const menuClose = document.createElement("img");
+        menuClose.src = "/shared/icon-close.svg";
+        menuClose.classList.add("mobile-menu__close");
+        menuClose.onclick = function () {
+            mobileMenuState = false;
+            mobileMenu.classList.add("hidden");
+        };
+        mobileMenu.append(menuClose);
 
-    const mobileNav = document.createElement('nav');
-    mobileNav.classList.add('mobile-nav', 'text-preset-8');
+        const mobileNav = document.createElement("nav");
+        mobileNav.classList.add("mobile-nav", "text-preset-8");
 
-    const navItems = document.createElement('ul');
-    navItems.classList.add('mobile-nav__nav');
+        const navItems = document.createElement("ul");
+        navItems.classList.add("mobile-nav__nav");
 
-    const navItemsArray = ['Home', 'Destination', 'Crew', 'Technology'];
-    navItemsArray.forEach((item, idx) => {
-      const navItem = document.createElement('li');
+        const navItemsArray = ["Home", "Destination", "Crew", "Technology"];
+        navItemsArray.forEach((item, idx) => {
+            const navItem = document.createElement("li");
 
-      const navLink = document.createElement('a');
-      navLink.classList.add('mobile-nav__nav-link');
-      if (item === 'Home') {
-        navLink.href = '/';
-      } else {
-        navLink.href = `/${item.toLowerCase()}`;
-      }
+            const navLink = document.createElement("a");
+            navLink.classList.add("mobile-nav__nav-link");
+            if (item === "Home") {
+                navLink.href = "/";
+            } else {
+                navLink.href = `/${item.toLowerCase()}`;
+            }
 
-      const numbering = document.createElement('span');
-      numbering.textContent = idx.toString().padStart(2, '0');
-      numbering.classList.add('mobile-nav__numbering');
+            const numbering = document.createElement("span");
+            numbering.textContent = idx.toString().padStart(2, "0");
+            numbering.classList.add("mobile-nav__numbering");
 
-      if (navLink.pathname === window.location.pathname) {
-        navLink.classList.add('active-link');
-      }
+            if (navLink.pathname === window.location.pathname) {
+                document
+                    .getElementsByClassName("active-link")[0]
+                    .classList.remove("active-link");
+                navLink.classList.add("active-link");
+            }
 
-      navLink.append(numbering, item);
-      navItem.append(navLink);
-      navItems.append(navItem);
-    });
+            navLink.append(numbering, item);
+            navItem.append(navLink);
+            navItems.append(navItem);
+        });
 
-    mobileNav.appendChild(navItems);
-    mobileMenu.appendChild(mobileNav);
-    body.append(mobileMenu);
-  }
+        mobileNav.appendChild(navItems);
+        mobileMenu.appendChild(mobileNav);
+        body.append(mobileMenu);
+    }
 };
 
-const navLinks = document.getElementsByClassName('nav__nav-link');
+const navLinks = document.getElementsByClassName("nav__nav-link");
 Array.prototype.forEach.call(navLinks, function (link) {
-  if (link.pathname === window.location.pathname) {
-    link.classList.add('active-link');
-  }
+    if (link.pathname === window.location.pathname) {
+        link.classList.add("active-link");
+    }
 });
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import {resolve} from "path";
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                destination: resolve(__dirname, "destination.html"),
+                crew: resolve(__dirname, "crew.html"),
+                technology: resolve(__dirname, "technology.html"),
+            },
+        },
+    },
+});
