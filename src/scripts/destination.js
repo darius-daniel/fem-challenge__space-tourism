3335fc89092a62340c48518f42beef496a8257b3
Darius Daniel <dariusdaniel771@email.com>
2025-01-07 13:20:11 +0100

    Optimized destination page for break point screen sizes

--- src/scripts/destination.js
+++ src/scripts/destination.js
@@ -1,46 +1,53 @@
 let currentDestination = "moon";
 const destinationNavItems = document.getElementsByClassName(
-    "destination-data__nav-list-item",
+  "destination-data__nav-list-item",
 );
 Array.prototype.forEach.call(destinationNavItems, function (item) {
-    item.onclick = async function () {
-        currentDestination = item.innerText.toLowerCase();
-        document
-            .getElementsByClassName("current-destination")[0]
-            .classList.remove("current-destination");
-        item.classList.add("current-destination");
+  item.onclick = async function () {
+    currentDestination = item.innerText.toLowerCase();
+    document
+      .getElementsByClassName("current-destination")[0]
+      .classList.remove("current-destination");
+    item.classList.add("current-destination");
+
+    document.getElementsByClassName(
+      "destination-data__article-heading",
+    )[0].innerText = currentDestination.toUpperCase();
+
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const destinations = data.destinations;
+        const destination = destinations.find(
+          (destination) =>
+            destination.name.toLowerCase() === currentDestination.toLowerCase(),
+        );
 
         document.getElementsByClassName(
+          "destination-data__article-desc",
+        )[0].innerHTML = destination.description;
+
+        document.getElementsByClassName(
+          "destination-data__article-desc",
+        )[0].style.minWidth =
+          document.getElementsByClassName(
             "destination-data__article-heading",
-        )[0].innerText = currentDestination.toUpperCase();
+          )[0].offsetWidth + "px";
 
-        fetch("./data.json")
-            .then((response) => response.json())
-            .then((data) => {
-                const destinations = data.destinations;
-                const destination = destinations.find(
-                    (destination) =>
-                        destination.name.toLowerCase() === currentDestination.toLowerCase(),
-                );
+        document.getElementsByClassName(
+          "destination-data__stat--distance",
+        )[0].innerHTML = destination.distance;
 
-                document.getElementsByClassName(
-                    "destination-data__article-desc",
-                )[0].innerHTML = destination.description;
+        document.getElementsByClassName(
+          "destination-data__stat--travel-time",
+        )[0].innerHTML = destination.travel;
 
-                document.getElementsByClassName(
-                    "destination-data__stat--distance",
-                )[0].innerHTML = destination.distance;
+        document.getElementsByClassName("destination-data__image")[0].src =
+          `./destination/image-${currentDestination}.png`;
+      });
+  };
 
-                document.getElementsByClassName(
-                    "destination-data__stat--travel-time",
-                )[0].innerHTML = destination.travel;
-
-                document.getElementsByClassName("destination-data__image")[0].src =
-                    `./destination/image-${currentDestination}.png`;
-            });
-    };
-
-    if (item.innerText.toLowerCase() === currentDestination.toLowerCase()) {
-        item.classList.add("current-destination");
-    }
+  if (item.innerText.toLowerCase() === currentDestination.toLowerCase()) {
+    item.classList.add("current-destination");
+  }
 });
