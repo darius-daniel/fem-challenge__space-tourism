1a3c47b2d881f292c1e7fa8b951cd5a425266d42
Darius Daniel <dariusdaniel771@email.com>
2025-01-07 12:46:39 +0100

    Optimized landing page for break point screen sizes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,12 @@
-document.getElementsByClassName("explore-btn")[0].addEventListener("click", () => {
+document
+  .getElementsByClassName("explore-btn")[0]
+  .addEventListener("click", () => {
     window.location.href = "/destination";
-})
+  });
+
+if (window.innerWidth >= 1025) {
+  const heroTitleLargeText = document.getElementsByClassName("hero__title")[0].lastElementChild;
+  const heroDesc = document.getElementsByClassName("hero__desc")[0];
+
+  heroDesc.style.width = heroTitleLargeText.offsetWidth + 'px';
+}
